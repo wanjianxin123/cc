64410fc4b03ec84c173221da2d555e462e755031
wanjianxin <1064357909@qq.com>
2018-01-24 17:03:41 +0800

    111

---
+++ src/main/java/com/wjx/edu/demo/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.wjx.edu.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+private String mm;
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
